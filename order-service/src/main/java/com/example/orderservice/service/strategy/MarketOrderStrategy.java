722366a09b063ef5ea51770c0ca66809ef2130c5
davidtsai <david.tsai@tpisoftware.com>
2022-08-24 21:28:07 +0800

    移除@Service標籤

--- order-service/src/main/java/com/example/orderservice/service/strategy/MarketOrderStrategy.java
+++ order-service/src/main/java/com/example/orderservice/service/strategy/MarketOrderStrategy.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-@Service
+
 public class MarketOrderStrategy implements IOrderStrategy {
 
     private LinkedList<Order> buyOrders;
